df6d96915b0f80c83045dda5d1cd80a6a3de3f52
Renato da Silva Viana <verkylen@gmail.com>
2022-09-03 19:31:04 -0300

    1ª versão final da tela 1

--- js/quizzList.js
+++ js/quizzList.js
@@ -1,1 +1,56 @@
-// const Request = axios.get();
+function directThirdPage() {
+    document.querySelector(".container-quizz-list").classList.add("hide");
+    document.querySelector(".container-create-quizz").classList.remove("hide");
+}
+  
+let allQuizzes = {};
+
+function directSecondPage(id) {
+    quizzToAnswer = allQuizzes[id];
+    loadQuizzScreen();
+}
+
+const request = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+
+request.then(listServerQuizzes);
+
+let userQuizzes;
+let userIDs = [];
+
+if (localStorage.length !== 0) {
+    userQuizzes = JSON.parse(localStorage.quizzes);
+    userIDs = userQuizzes.map((quizz) => {return quizz.id});
+
+    document.querySelector(".no-quizzes").classList.add("hide");
+    document.querySelector(".your-quizzes").classList.remove("hide");
+
+    let listUser = "";
+    for (let card of userQuizzes) {
+        listUser += `<figure onclick="directSecondPage(${card.id})" data-identifier="quizz-card">
+                         <img src="${card.image}" alt="Imagem não suportada ou indisponível"/>
+                         <figcaption>
+                             ${card.title}
+                         </figcaption>
+                     </figure>`;
+
+        allQuizzes[card.id] = card;
+    }
+    document.querySelector(".your-quizzes div").innerHTML = listUser;
+}
+
+function listServerQuizzes(response) {
+    let listAll = "";
+    for (let card of response.data) {
+        if (!userIDs.includes(card.id)) {
+            listAll += `<figure onclick="directSecondPage(${card.id})" data-identifier="quizz-card">
+                            <img src="${card.image}" alt="Imagem não suportada ou indisponível"/>
+                            <figcaption>
+                                ${card.title}
+                            </figcaption>
+                        </figure>`;
+
+            allQuizzes[card.id] = card;
+        }
+    }
+    document.querySelector(".all-quizzes div").innerHTML = listAll;
+}
